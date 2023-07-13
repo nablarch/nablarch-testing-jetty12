18f8adb4cc745490e9f5be5bb7d8d3915285d70e
Takayuki Uchida <jacobitebeerlove@gmail.com>
2023-07-13 11:29:43 +0900

    nablarch-testingのHTTPメソッド指定機能を使用するよう変更しました。 (#3)
    * feat: nablarch-testingのHTTPメソッド指定機能を使用するよう変更
    
    * Revert "feat: nablarch-testingのHTTPメソッド指定機能を使用するよう変更"
    
    This reverts commit 47f209f21ef0d6ac9ebb89a0d642d77608a9f948.
    
    修正方法に誤りがあったため、一旦revertして再修正する
    
    * test: nablarch-testingのHTTPメソッド指定機能を使用するよう変更、コンテナが返却するステータスとFWが設定するステータスが異なる場合のテスト追加
    
    * refactor: 不要なimportを削除
    
    * doc: nablarch-testing-jetty6レビュー指摘内容取込
    
    * doc: レビュー指摘修正
    
    ---------
    
    Co-authored-by: Takayuki Uchida <uchida.takayuki@tis.co.jp>

--- src/test/java/nablarch/test/core/http/BasicHttpRequestTestTemplateTest.java
+++ src/test/java/nablarch/test/core/http/BasicHttpRequestTestTemplateTest.java
@@ -1,18 +1,14 @@
 package nablarch.test.core.http;
 
-import nablarch.core.util.StringUtil;
 import nablarch.fw.ExecutionContext;
 import nablarch.fw.web.HttpRequest;
 import nablarch.fw.web.HttpResponse;
 import nablarch.fw.web.HttpServer;
-import nablarch.fw.web.MockHttpRequest;
 import nablarch.fw.web.httpserver.HttpServerJetty12;
 import nablarch.test.RepositoryInitializer;
 import org.junit.After;
 import org.junit.Test;
 
-import java.util.List;
-import java.util.Map;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertThat;
@@ -104,30 +100,12 @@ public class BasicHttpRequestTestTemplateTest {
         target.execute("overlayFirst");   // 1番目のWebAppであるappのリソースを取得できること。
         target.execute("overlaySecond");  // 2番目のWebAppであるappのリソースを取得できること。
         target.execute("overlayThird");   // 3番目のWebAppであるappのリソースを取得できること。
+        target.execute("overlayNotFound"); // 存在しないリソースは取得できないこと。
     }
 
 
-    /**
-     * テスト用の{@link BasicHttpRequestTestTemplate}
-     * <p>
-     * このクラスは、HTTPメソッドをテストデータで指定できるように拡張している。<br>
-     * これは、Jetty12が静的リソースへのアクセスをPOSTでできなくなったことに起因する。
-     * </p>
-     * <p>
-     * Jetty9までは、POSTメソッドでも静的リソースへアクセスできた。これは、DefaultServletのdoPostメソッドの
-     * 実装がdoGetメソッドに処理を委譲する形になっていたためできていた。<br>
-     * しかし、Jetty12のDefaultServletはdoPostを実装しなくなったため、静的リソースにPOSTメソッドで
-     * アクセスしようとすると405 Method Not Allowedのエラーが発生するようになった。
-     * </p>
-     * <p>
-     * 本来はNTFを改良して任意のHTTPメソッドを指定できるようにすべきだが、
-     * クエリパラメータの扱いをどうすべきかなど考慮しなければならないことが多いため、
-     * 2023年の対応では一旦アドホックな対応で済ませることにした。<br>
-     * NTFが改良されて標準機能として任意のHTTPメソッドを指定できるようになった場合は、
-     * そちらを利用する方法に修正すること。
-     * </p>
-     */
-    private static class BasicHttpRequestTestTemplateForTesting extends AbstractHttpRequestTestTemplate<BasicHttpRequestTestCaseInfo> {
+    /** テスト用の{@link BasicHttpRequestTestTemplate} */
+    private static class BasicHttpRequestTestTemplateForTesting extends BasicHttpRequestTestTemplate {
 
         private BasicHttpRequestTestTemplateForTesting(Class<?> testClass) {
             super(testClass);
@@ -142,62 +120,20 @@ public class BasicHttpRequestTestTemplateTest {
         protected HttpServer createHttpServer() {
             return new HttpServerForTesting();
         }
-
-        @Override
-        protected HttpRequest createHttpRequest(BasicHttpRequestTestCaseInfo testCaseInfo) {
-            final MockHttpRequest httpRequest = (MockHttpRequest)super.createHttpRequest(testCaseInfo);
-            httpRequest.setMethod(testCaseInfo.getHttpMethod());
-            return httpRequest;
-        }
-
-        @Override
-        protected BasicHttpRequestTestCaseInfo createTestCaseInfo(
-                String sheetName,
-                Map<String, String> testCaseParams,
-                List<Map<String, String>> contexts,
-                List<Map<String, String>> requests,
-                List<Map<String, String>> expectedResponses,
-                List<Map<String, String>> cookie) {
-            return new BasicHttpRequestTestCaseInfo(sheetName,
-                    testCaseParams,
-                    contexts,
-                    requests,
-                    expectedResponses,
-                    cookie);
-        }
-    }
-
-    /** {@link TestCaseInfo}を、HTTPメソッドを指定できるように拡張したクラス。 */
-    private static class BasicHttpRequestTestCaseInfo extends TestCaseInfo {
-        /** コンテキスト */
-        private final List<Map<String, String>> context;
-
-        public BasicHttpRequestTestCaseInfo(
-                String sheetName,
-                Map<String, String> testCaseParams,
-                List<Map<String, String>> context,
-                List<Map<String, String>> request,
-                List<Map<String, String>> expectedResponseListMap,
-                List<Map<String, String>> cookie) {
-            super(sheetName, testCaseParams, context, request, expectedResponseListMap, cookie);
-            this.context = context;
-        }
-
-        /**
-         * テスト対象とするリクエストのHTTPメソッドを返却する。
-         *
-         * @return HTTPメソッド
-         */
-        public String getHttpMethod() {
-            String httpMethod = context.get(0).get("HTTP_METHOD");
-            return StringUtil.hasValue(httpMethod) ? httpMethod : "POST";
-        }
     }
 
     /** テスト用HttpServer */
     private static class HttpServerForTesting extends HttpServerJetty12 {
 
-        /** {@inheritDoc} */
+        /** {@inheritDoc}
+         *  {@link HttpRequestTestSupportHandler#handle}内で、後続ハンドラから返却された
+         *  {@link HttpResponse}のステータスコードを内部に保持している。
+         *  この時、保持されるステータスコードはNablarchが設定したステータスコードである。
+         *  しかし、コンテナがNablarchの設定したステータスコードとは異なるステータスコードを返却する場合がある。
+         *  (サーブレットフォワード先のコンテンツが見つからない場合などはNablarchは200を返すがコンテナは404を返そうとする。)
+         *  ここではコンテナが返却したステータスコードを検証に使用したいため、{@link HttpServer}が返却したレスポンスを
+         *  解析した後、ここで改めて{@link HttpRequestTestSupportHandler}のステータスコードにセットしている。
+         */
         @Override
         public HttpResponse handle(HttpRequest req, ExecutionContext ctx) {
             HttpResponse res = super.handle(req, ctx);
